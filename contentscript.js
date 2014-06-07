30fcce3282db3364ba03f729d2ded3c63049527f
Chakradar Raju <charkadarraju@gmail.com>
2014-06-07 16:56:30 +0530

    Adding emoticons support

--- contentscript.js
+++ contentscript.js
@@ -2,16 +2,68 @@ window.onerror = function(e) {
   console.log(e);
 }
 
+var selector = {
+  editor: "div[class='inline_editor_form'] div[interactive=true]",
+  submit: "a[class='submit_button']",
+  messages: "div[class='inbox_message_text']"
+};
+
 function attachEventHandler() {
-  document.querySelector("div[class='inline_editor_form'] div[interactive=true]").onkeypress = eventHandler;
+  document.querySelector(selector.editor).onkeypress = eventHandler;
 }
 
 function eventHandler(e) {
   if (e.ctrlKey == false && e.charCode == 13) {
-    document.querySelector("a[class='submit_button']").click();
+    document.querySelector(selector.submit).click();
     e.preventDefault();
     attachEventHandler();
   }
 };
 
+function replaceAllEmoticonsInPage() {
+  var messages = document.querySelectorAll(selector.messages);
+  for (var i = 0; i < messages.length; i++) {
+    replaceEmoticonsInMessage(messages[i]);
+  }
+}
+
+function replaceEmoticonsInMessage(message) {
+  var newContent, newContentLength;
+  for (var i = 0; i < message.childNodes.length; i++) if (message.childNodes[i].nodeType == 3) {
+    newContent = replaceEmoticonsInText(message.childNodes[i]);
+    newContentLength = newContent.length;
+    for (var j = 0; j < newContentLength; j++) {
+      message.insertBefore(newContent[0], message.childNodes[i++]);
+    }
+    message.removeChild(message.childNodes[i--]);
+  }
+}
+
+var smileys = {
+  happy: /:\)/g,
+  sad: /:\(/g,
+  laugh: /:D/g,
+  nimpressed: /:\//g,
+  dcare: /:\|/g,
+  irritate: /:p/g,
+  wink: /;\)/g,
+  koku: /:P/g,
+  cry: /:'\(/g
+}
+
+function replaceEmoticonsInText(text) {
+  var newContent = text.data;
+  for (name in smileys) {
+    newContent = newContent.replace(smileys[name], "<div class='smiley smiley-"+name+"'></div>");
+  }
+  return createNodes(newContent);
+}
+
+function createNodes(content) {
+  var node = document.createElement("div");
+  node.innerHTML = content;
+  return node.childNodes;
+}
+
 attachEventHandler();
+replaceAllEmoticonsInPage();
